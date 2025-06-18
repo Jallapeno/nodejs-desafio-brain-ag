08219094cdfbc8ffe947cacf3c607f461a69a5f9
Hytalo <hytalo48@gmail.com>
2025-06-17 22:02:09 -0300

    feat: add updateRuralProperty method to RuralPropertyController

--- src/controllers/rural-property.controller.ts
+++ src/controllers/rural-property.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Inject, Patch, Post } from "@nestjs/common";
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
-import { CREATE_RURAL_PROPERTY_USE_CASE } from "src/constants/constants";
+import { CREATE_RURAL_PROPERTY_USE_CASE, UPDATE_RURAL_PROPERTY_USE_CASE } from "src/constants/constants";
 import { CreateRuralPropertyDto } from "src/infra/dtos/rural-property/create-rural-property-infra.dto";
+import { UpdateRuralPropertyDto } from "src/infra/dtos/rural-property/update-rural-property-infra.dto";
 import { CreateRuralPropertyUseCase } from "src/usecases/rural-property/create-rural-property.usecase";
 
 @Controller("rural-property")
@@ -10,8 +11,8 @@ export class RuralPropertyController {
   constructor(
     @Inject(CREATE_RURAL_PROPERTY_USE_CASE)
     private readonly _createRuralPropertyUseCase: CreateRuralPropertyUseCase,
-    // @Inject(UPDATE_RURAL_PROPERTY_USE_CASE)
-    // private readonly updateRuralPropertyUseCase: UpdateRuralPropertyUseCase,
+    @Inject(UPDATE_RURAL_PROPERTY_USE_CASE)
+    private readonly _updateRuralPropertyUseCase: UpdateRuralPropertyUseCase,
     // @Inject(DELETE_RURAL_PROPERTY_USE_CASE)
     // private readonly deleteRuralPropertyUseCase: DeleteRuralPropertyUseCase,
     // @Inject(LIST_RURAL_PROPERTY_BY_PRODUCER_ID_USE_CASE)
@@ -36,5 +37,23 @@ export class RuralPropertyController {
   @ApiBody({ type: CreateRuralPropertyDto })
   async createRuralProperty(@Body() body: CreateRuralPropertyDto) {
     return await this._createRuralPropertyUseCase.execute(body);
+  }
+
+  @Patch(":id")
+  @ApiOperation({
+    summary: "Update an existing rural property",
+    description: "Updates the details of an existing rural property."
+  })
+  @ApiResponse({
+    status: 200,
+    description: "Rural property updated successfully."
+  })
+  @ApiResponse({
+    status: 400,
+    description: "Bad request. Invalid input data."
+  })
+  @ApiBody({ type: UpdateRuralPropertyDto })
+  async updateRuralProperty(@Body() body: UpdateRuralPropertyDto) {
+    return await this._updateRuralPropertyUseCase.execute(body);
   }
 }
